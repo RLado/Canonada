740352d3d58962734cfcc4e480d1e5dbb132ce4f
Ricard Lado, PhD <ricard@lado.one>
2025-03-07 09:12:38 +0000

    Update project metadata and add development dependencies

--- src/canonada/__init__.py
+++ src/canonada/__init__.py
@@ -1,3 +1,9 @@
 """
 Canonada
+
+A data science framework that helps you build production-ready streaming pipelines for data processing in Python
 """
+
+from . import catalog as catalog
+from . import pipeline as pipeline
+from . import system as system
