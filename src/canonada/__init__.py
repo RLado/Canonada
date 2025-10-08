5950757a6ca644d7e325d105246caa540311bb9e
Ricard Lado <ricard@lado.one>
2025-10-08 20:19:57 +0200

    Set multiprocessing start method to 'fork'
    - Python 3.14 changed its default multiprocessing start method from "fork" to "forkserver", breaking functionality.

--- src/canonada/__init__.py
+++ src/canonada/__init__.py
@@ -4,7 +4,17 @@ Canonada
 A data science framework that helps you build production-ready streaming pipelines for data processing in Python
 """
 
-from . import exceptions as exceptions
+import multiprocessing
+import platform
+import sys
+
 from . import catalog as catalog
+from . import exceptions as exceptions
 from . import pipeline as pipeline
 from . import system as system
+
+# Python 3.14 changed its default multiprocessing start method from "fork" to "forkserver", breaking functionality
+if sys.version_info >= (3, 14):
+    # Set fork on supported platforms (Linux)
+    if platform.system() == "Linux":
+        multiprocessing.set_start_method("fork", force=True)
